5df1ee5931ea97814f0260cf53ea0704fb59f092
Douglas Gardner <douglas@chippy.ch>
2018-02-15 17:42:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# creditor
+
+Quickly written tool to output your dependencies to a CSV format.
+
+## Installation
+```
+$ npm install -g creditor
+```
+
+## Usage
+`cd` into the root directory with your `package.json` file, then issue:
+```
+$ creditor > dependencies.csv
+```
+
+This tool accepts no arguments.
---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var creditsTo = require('credits-to')
+var toCSV = require('array-to-csv')
+
+creditsTo(function (err, credits) {
+	if (err) return console.error(err)
+	var npmCredits = credits.npm
+	var output = []
+	for (packageName in npmCredits) {
+		var credit = npmCredits[packageName]
+		if (!credit.versions) credit.versions = []
+		if (!credit.licenses) credit.licenses = []
+		console.log(credit)
+		output.push([credit.name, credit.description, credit.versions.join(';'), credit.licenses.join(';')])
+	}
+	console.log(toCSV(output))
+})
